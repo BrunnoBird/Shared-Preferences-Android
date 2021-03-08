ceb4092b7bed11ea19ac72b3a0861502cdf4f396
BrunnoBird <63742776+BrunnoBird@users.noreply.github.com>
2021-03-08 19:24:55 -0300

    First

---
+++ app/src/main/java/com/example/preferenciasdousuario/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.preferenciasdousuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class MainActivity extends AppCompatActivity {
+
+        private Button buttonSalvar;
+        private TextInputEditText editNome;
+        private TextView textResultado;
+        private static final String ARQUIVO_PREFERENCIA = "ArquivoPreferencia"; //static sempre sera o mesmo independente da instancia
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonSalvar = findViewById(R.id.buttonSalvar);
+        editNome = findViewById(R.id.editNome);
+        textResultado = findViewById(R.id.textResultado);
+
+        buttonSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //salvar os dados no aparelho, essa classe ela cria um arquivo XML
+                SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIA, 0); // mode: 0  = modo privado, somente o nosso aplicativo salva e lê este arquivo
+                SharedPreferences.Editor editor = preferences.edit(); // conseguiremos editar o nosso arquivo de preferencias
+
+                //validar o nome
+                if(editNome.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "Preencha o nome", Toast.LENGTH_LONG).show();
+                } else {
+                    String nome = editNome.getText().toString();
+                    editor.putString("nome", nome); //chave e valor para salvar
+                    editor.commit(); // quando realmente o dado salva.
+                    textResultado.setText("Olá, " + nome);
+                }
+            }
+        });
+
+        // Recuperar dados salvos
+        SharedPreferences preferences = getSharedPreferences(ARQUIVO_PREFERENCIA, 0);
+
+        //Valida se temos o nome preferencias
+        if(preferences.contains("nome")){
+            String nome = preferences.getString("nome", "usuario não definido");
+            textResultado.setText("Olá, " + nome);
+        } else {
+            textResultado.setText("Olá, usuario não definido");
+        }
+    }
+}
